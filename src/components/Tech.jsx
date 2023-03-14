2f4a76f959c145d928410743f41edba7175cfa5c
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-03-14 13:55:32 -0300

    feat: experience, tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { BallCanvas} from './canvas'
+import {SectionWrapper} from "../hoc"
+import {technologies} from '../constants'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className=' flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology)=> (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
