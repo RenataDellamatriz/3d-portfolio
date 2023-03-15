e1861370c665977c04b044050ead41300c01fc40
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-03-15 11:13:32 -0300

    fix: contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,8 +28,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        "service_zbrxeqt",
-        "template_qey5m14",
+        process.env.VITE_APP_EMAILJS_SERVICE_ID,
+        process.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Renata",
@@ -37,7 +37,7 @@ const Contact = () => {
           to_email: "r.dellamatriz@hotmail.com",
           message: form.message,
         },
-        "W5CvIyP7cKOo0Jt7L"
+        process.send.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(() => {
         setLoading(false);
